af4f331801f1f2b7b1757eb9824836a4913a53c4
vitaliy <andv@kvarta.kz>
2022-03-15 15:51:02 +0100

    New commit

---
+++ mongoDB.py
@@ -0,0 +1,37 @@
+import pymongo
+import pprint
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")  # подключаемся к серверу
+mydb = myclient["NewBD"]  # получаем доступ к базе данных
+
+dblist = myclient.list_database_names()  # список баз данных
+print(dblist)
+
+mycol = mydb['newTab']  # создаем новую коллекцию или таблицу в БД
+
+#добавляем один элемент
+mydict = {"name": "Andrey", 'alt': 16}
+result = mycol.insert_one(mydict)
+print(result)
+print(result.inserted_id)
+print()
+
+#добавляем несколько элементов
+mydict = [{"name": "Arina", 'alt': 3},{"name": "Alyona", 'alt': 13}]
+result = mycol.insert_many(mydict)
+print(type(result))
+print(result.inserted_ids)
+
+# for x in result:
+#     print(x)
+
+
+
+# nameCol = str(input('Введите название таблицы'))
+#
+# collist = mydb.list_collection_names()
+# if nameCol in collist:
+#     print('ich finde')
+#
+# print(mydb.list_collection_names())
+# print(mycol.list_collection_names())
