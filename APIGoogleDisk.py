9d0c309981dff53350dc697a1ebba99a82c88924
vitaliy <andv@kvarta.kz>
2022-03-19 18:53:22 +0100

    Google Disk API Test

--- APIGoogleDisk.py
+++ APIGoogleDisk.py
@@ -1,9 +1,17 @@
+
 from google.oauth2 import service_account
 from googleapiclient.http import MediaIoBaseDownload,MediaFileUpload
 from googleapiclient.discovery import build
 import pprint
 import io
 import os.path
+
+import tkinter.filedialog as fd
+
+
+
+
+
 
 pp = pprint.PrettyPrinter(indent=4)
 #
@@ -25,8 +33,14 @@ service = build('drive', 'v3', credentials=credentials)
 #         results['files'] = results['files'] + nextPage['files']
 # pp.pprint(results.get('files'))
 
+
+# сохранение скачанного с диска файла в папку
 file_id = '1Der2vXRQDraJCSSucgVVzWLPO0B2o8fQ'
 request = service.files().get_media(fileId=file_id)
+
+file_object = service.files().get(fileId='1Der2vXRQDraJCSSucgVVzWLPO0B2o8fQ').execute()
+print(file_object)
+
 fh = io.BytesIO()
 downloader = MediaIoBaseDownload(fh, request)
 done = False
@@ -34,3 +48,11 @@ while done is False:
     status, done = downloader.next_chunk()
     print("Download %d%%." % int(status.progress() * 100))
 
+dirName = '/Users/vitaliy/Desktop/Python'
+
+#Вызов диалога выбора пути файла
+dirName = fd.askdirectory(title='Choose directory', initialdir=dirName)
+
+
+with open(os.path.join(dirName, file_name), 'wb') as wb:
+    wb.write(fh.getvalue())
